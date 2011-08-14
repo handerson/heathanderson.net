e5297a5af5f61be94a25c26194ee140876c9d591
Heath Anderson <handerson@blacksnowmedia.com>
2011-08-14 11:34:58 -0400

    two new rake posts

--- rakefile.rb
+++ rakefile.rb
@@ -1,12 +1,13 @@
 
 
-#http://blog.hendrikvolkmer.de/2011/02/25/moving-from-wordpress-on-a-vps-to-jekyll-and-amazon-s3/
+# based on http://blog.hendrikvolkmer.de/2011/02/25/moving-from-wordpress-on-a-vps-to-jekyll-and-amazon-s3/
 task :default => :deploy
 
 desc "Deploy to S3"
 task :deploy do
   sh "jekyll"
-  sh "s3cmd sync _site/* s3://www.heathanderson.net"
+  sh "s3cmd sync --reduced-redundancy --exclude 'images/*' _site/* s3://www.heathanderson.net"
+  sh "s3cmd sync --reduced-redundancy --add-header 'Expires: Thu, 6 Feb 2020 00:00:00 GMT' _site/images/* s3://www.heathanderson.net/images/"
 end
 
 
@@ -23,7 +24,7 @@ task :new_post, :title do |t, args|
    post.puts "---"
    post.puts "layout: post"
    post.puts "title: \"#{title.gsub(/&/,'&amp;')}\""
-   post.puts "date: #{Time.now.strftime('%Y-%m-%d %H:%M')}"
+   post.puts "date: #{Time.now.strftime('%Y-%m-%d')}"
    post.puts "published: false"
    post.puts "---"
  end
